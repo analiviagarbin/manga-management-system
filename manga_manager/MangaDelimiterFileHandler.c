098230e33aa5a8000409f6acaf4186e5c59e368f
Ana Flor Oliveira De Stefani <109155318+AFlor-OS@users.noreply.github.com>
2024-05-23 14:22:45 -0300

    Update MangaDelimiterFileHandler.c

--- manga_manager/MangaDelimiterFileHandler.c
+++ manga_manager/MangaDelimiterFileHandler.c
@@ -5,79 +5,71 @@
 #include "MangaDelimiterFileHandler.h"
 
 Manga mangas[MAX_MANGAS];
-int mangaCount = 0;
+int mC = 0; // manga counter 
 
-// Function readString reads string inputs
-void readString(char *prompt, char *str, int length) {
-    printf("%s", prompt);
-    fgets(str, length, stdin);
-    str[strcspn(str, "\n")] = '\0'; // Remove newline character
-}
-
-
-// Function readInt reads int inputs
-void readInt(char *prompt, int *num) {
-    printf("%s", prompt);
+// Function rInt reads int inputs
+void rInt(char *p, int *num) {
+    printf("%s", p);
     scanf("%d", num);
     getchar(); // Consume newline character left by scanf
 }
 
+// Function rString reads string inputs
+void rString(char *p, char *str, int len) {
+    printf("%s", p);
+    fgets(str, len, stdin);
+    str[strcspn(str, "\n")] = '\0';  // Remove newline character
+}
 
-// Function addManga adds a new manga to the list
+
+// adds a new manga to the list
 void addManga() {
-    if (mangaCount >= MAX_MANGAS) {
-        printf("Your library is full!\n");
+    if (mC >= MAX_MANGAS) {
+        printf("Error: Your library is full!\n");
         return;
-    }
+    } 
 
-    Manga m; // new manga
+    // new manga
+    Manga m; 
     printf("Enter the data: \n");
     readString("ISBN: ", m.ISBN, 20);
-    readString("Title: ", m.title, MAX_TITLE_LEN);
-    readString("Author: ", m.author, MAX_AUTHOR_LEN);
+    readString("Title: ", m.title, MAX_TITLE);
+    readString("Author: ", m.author, MAX_AUTHOR);
     readInt("Start Year: ", &m.startYear);
     readInt("End Year: ", &m.endYear);
-    readString("Genre: ", m.genre, MAX_GENRE_LEN);
-    readString("Magazine: ", m.magazine, MAX_MAGAZINE_LEN);
-    readString("Publisher: ", m.publisher, MAX_PUBLISHER_LEN);
+    readString("Genre: ", m.genre, MAX_GENRE);
+    readString("Magazine: ", m.magazine, MAX_MAGAZINE);
+    readString("Publisher: ", m.publisher, MAX_PUBLISHER);
     readInt("Edition Year: ", &m.editionYear);
     readInt("Total Volumes: ", &m.totalVolumes);
     readInt("Acquired Volumes: ", &m.acquiredVolumes);
 
     for (int i = 0; i < m.acquiredVolumes; i++) {
         printf("Enter Volume %d: ", i + 1);
-        scanf("%d", &m.acquiredVolumeList[i]); // receives the informed quantity of volumes sold
+        scanf("%d", &m.acquiredVolumeList[i]); // receives the informed quantity of volumes 
     }
-    getchar(); // enter
+    getchar(); 
 
-    mangas[mangaCount++] = m; // increments the manga counter
     printf("Manga successfully added to your library!\n");
+    mangas[mC++] = m; 
 }
 
 
-// Function listMangas lists the mangas uploaded according to the counter mangaCount
-void listMangas() {
-    for (int i = 0; i < mangaCount; i++) {
-        printf("Manga %d: %s (%s)\n", i + 1, mangas[i].title, mangas[i].ISBN);
-    }
-}
-
-
-// Function updateManga updates the manga according to the ISBN
+// Function updates the manga according to the ISBN
 void updateManga() {
     char ISBN[20];
-    readString("Enter ISBN of the manga to update: ", ISBN, 20);
+    readString("Enter ISBN of the manga that you want to update: ", ISBN, 20);
 
-    for (int i = 0; i < mangaCount; i++) {
+    for (int i = 0; i < mC; i++) {
         if (strcmp(mangas[i].ISBN, ISBN) == 0) {
             printf("Enter the new data: \n");
-            readString("Title: ", mangas[i].title, MAX_TITLE_LEN);
-            readString("Author: ", mangas[i].author, MAX_AUTHOR_LEN);
+            readString("Title: ", mangas[i].title, MAX_TITLE);
+            readString("Author: ", mangas[i].author, MAX_AUTHOR);
             readInt("Start Year: ", &mangas[i].startYear);
             readInt("End Year: ", &mangas[i].endYear);
-            readString("Genre: ", mangas[i].genre, MAX_GENRE_LEN);
-            readString("Magazine: ", mangas[i].magazine, MAX_MAGAZINE_LEN);
-            readString("Publisher: ", mangas[i].publisher, MAX_PUBLISHER_LEN);
+            readString("Genre: ", mangas[i].genre, MAX_GENRE);
+            readString("Magazine: ", mangas[i].magazine, MAX_MAGAZINE);
+            readString("Publisher: ", mangas[i].publisher, MAX_PUBLISHER);
             readInt("Edition Year: ", &mangas[i].editionYear);
             readInt("Total Volumes: ", &mangas[i].totalVolumes);
             readInt("Acquired Volumes: ", &mangas[i].acquiredVolumes);
@@ -96,23 +88,30 @@ void updateManga() {
     printf("You don't have this manga in your library yet!\n");
 }
 
+// Function lists the mangas uploaded according to the counter mangaCount
+void listMangas() {
+    for (int i = 0; i < mC; i++) {
+        printf("Manga %d: %s (%s)\n", i + 1, mangas[i].title, mangas[i].ISBN);
+    }
+}
 
-// Function deleteManga delete the manga according to the ISBN
+
+// Function delete the manga according to the ISBN
 void deleteManga() {
     char ISBN[20];
-    readString("Enter ISBN of the manga to delete: ", ISBN, 20);
+    readString("Enter ISBN of the manga you want to delete: ", ISBN, 20);
 
-    for (int i = 0; i < mangaCount; i++) {
+    for (int i = 0; i < mC; i++) {
         if (strcmp(mangas[i].ISBN, ISBN) == 0) {
             printf("Are you sure you want to delete %s? (y|Y/n): ", mangas[i].title);
-            char confirm;
-            scanf(" %c", &confirm);
+            char sure;
+            scanf(" %c", &sure);
             getchar();
-            if (confirm == 'y' || confirm == 'Y') {
-                for (int j = i; j < mangaCount - 1; j++) {
+            if (sure == 'y' || sure == 'Y') {
+                for (int j = i; j < mC - 1; j++) {
                     mangas[j] = mangas[j + 1];
                 }
-                mangaCount--;
+                mC--;
                 printf("Manga deleted successfully.\n");
             } else {
                 printf("Deletion cancelled.\n");
@@ -121,7 +120,7 @@ void deleteManga() {
         }
     }
 
-    printf("You don't have this manga in your library yet!\n");
+    printf("Error: You don't have this manga in your library yet!\n");
 }
 
 
@@ -129,7 +128,7 @@ void deleteManga() {
 void savePrimaryIndex() {
     FILE *file = fopen("primary_index.txt", "w");
     if (file) {
-        for (int i = 0; i < mangaCount; i++) {
+        for (int i = 0; i < mC; i++) {
             fprintf(file, "%s %d\n", mangas[i].ISBN, i);
         }
         fclose(file);
@@ -143,7 +142,7 @@ void savePrimaryIndex() {
 void saveSecondaryIndex() {
     FILE *file = fopen("secondary_index.txt", "w");
     if (file) {
-        for (int i = 0; i < mangaCount; i++) {
+        for (int i = 0; i < mC; i++) {
             fprintf(file, "%s %d\n", mangas[i].title, i);
         }
         fclose(file);
@@ -160,7 +159,7 @@ void saveData() {
 
     FILE *file = fopen("mangas.txt", "w");
     if (file) {
-        for (int i = 0; i < mangaCount; i++) {
+        for (int i = 0; i < mC; i++) {
             Manga m = mangas[i];
             fprintf(file, "%s|%s|%s|%d|%d|%s|%s|%s|%d|%d|%d|",
                     m.ISBN, m.title, m.author, m.startYear, m.endYear,
@@ -185,7 +184,7 @@ void saveData() {
 void loadData() {
     FILE *file = fopen("mangas.txt", "r");
     if (file) {
-        mangaCount = 0;
+        mC = 0;
         while (!feof(file)) {
             Manga m;
             char volumes[MAX_RECORD_SIZE];
@@ -199,7 +198,7 @@ void loadData() {
                     m.acquiredVolumeList[j++] = atoi(token);
                     token = strtok(NULL, " ");
                 }
-                mangas[mangaCount++] = m;
+                mangas[mC++] = m;
             }
         }
         fclose(file);
